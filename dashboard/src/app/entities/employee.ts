13b86f6a2a04bb36872c22a62b8528e5f8f64c79
circoloco <asvimalo@gmail.com>
2017-11-09 23:30:00 +0100

    Entities employee

---
+++ dashboard/src/app/entities/employee.ts
@@ -0,0 +1,18 @@
+export class Employee {
+  id: number;
+  firstName:string;
+  lastName:string;
+  personNr:string;
+  acquiredKnowledges: Array<object>;
+  assignments: Array<object>;
+
+}
+export class EmployeePost {
+
+  firstName:string;
+  lastName:string;
+  personNr:string;
+  newKnowledge:string;
+  knowledges: Array<object>;
+  
+}
