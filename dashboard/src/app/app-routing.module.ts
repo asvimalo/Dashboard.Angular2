ba7d616f9c1fbcb9b68e1719604899cd55df0b2f
circoloco <asvimalo@gmail.com>
2017-11-11 15:18:27 +0100

    Changing routing flow with detail components

--- dashboard/src/app/app-routing.module.ts
+++ dashboard/src/app/app-routing.module.ts
@@ -3,12 +3,16 @@ import { RouterModule, Routes } from '@angular/router';
 import { EmployeesComponent } from './employees/employees.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { ProjectsComponent } from './projects/projects.component';
+import { EmployeeDetailComponent } from './employee-detail/employee-detail.component'
+import { ProjectDetailComponent } from './project-detail/project-detail.component'
 
 const routes: Routes = [
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
   { path: 'dashboard', component: DashboardComponent },
   { path: 'employees', component: EmployeesComponent },
+  { path: 'employee-detail/:id', component: EmployeeDetailComponent },
   { path: 'projects', component: ProjectsComponent },
-  { path: '', redirectTo: '/dashboard', pathMatch: 'full' }
+  { path: 'project-detail/:id', component: ProjectDetailComponent },
 ];
 @NgModule({
   imports: [ RouterModule.forRoot(routes) ],
