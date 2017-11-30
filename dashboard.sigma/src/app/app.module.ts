2a1b68f2cf60dfb6de83e441bca66172ef245fb7
asvimalo <asvimalo@gmail.com>
2017-11-30 11:01:28 +0100

    Admin component

--- dashboard.sigma/src/app/app.module.ts
+++ dashboard.sigma/src/app/app.module.ts
@@ -1,8 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
+import { AdminModule } from './admin/admin.module';
 import { AppRoutingModule } from './app-routing.module';
 
+import { AdminComponent } from './admin/admin.component';
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { EmployeeComponent } from './employee/employee.component';
@@ -12,13 +16,17 @@ import { ProjectComponent } from './project/project.component';
 @NgModule({
   declarations: [
     AppComponent,
+    AdminComponent,
     DashboardComponent,
     EmployeeComponent,
     ProjectComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+     BrowserModule,
+    FormsModule,
+    HttpModule,
+    AdminModule,
+    AppRoutingModule     
   ],
   providers: [],
   bootstrap: [AppComponent]
